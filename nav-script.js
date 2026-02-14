7ba385c22c0b1b2f9b49724a9e63ac6219fa9866
Nirmal Singh <nirmalsinghh007@gmail.com>
2026-02-14 19:45:26 +0530

    Refactor app and features added

--- nav-script.js
+++ nav-script.js
@@ -1,8 +1,49 @@
+import blogsData from "./data.js"
+
 const currentPath = document.location.pathname
+const blogsContainer = document.getElementById('blogs-container')
+const hamburgerEl = document.getElementById('hamburger')
+const closeBtnEl = document.getElementById('close-btn')
+const sidebar = document.getElementById('sidebar')
 
 const tag = document.querySelector(`a[href="${currentPath}"]`)
 
-if(tag.parentElement.tagName === "LI"){
-    tag.classList.add("current")
+// if(tag.parentElement.tagName === "LI"){
+//     tag.classList.add("current")
+// }
+
+// sidebar handling
+
+hamburgerEl.addEventListener('click', ()=> {
+    console.log("clicked")
+    sidebar.style.transform = "translateX(0%)"
+})
+
+closeBtnEl.addEventListener('click', ()=> {
+    sidebar.style.transform = 'translateX(100%)'
+})
+
+
+const getThreeBlogs = () => {
+    let blogs = ``
+
+    for(let i = 0; i<blogsData.length; i++){
+        const {date, img, alt, name, content} = blogsData[i]
+        if(i < 3){
+            blogs += `
+            <div class="blog">
+                <img src="./images/${img}" alt="${alt}" width="375" height="250">
+                <span class="date">${date}</span>
+                <h3>${name}</h3>
+                <p class="blog-content">${content}</p>
+            </div>
+            `
+        } 
+    }
+    return blogs
+}
+const renderBlogsData = () => {
+    blogsContainer.innerHTML = getThreeBlogs()
 }
 
+renderBlogsData()
--- script.js
+++ script.js
@@ -3,6 +3,10 @@ import blogsData from "./data.js"
 // DOM Elements
 const blogsContainer = document.getElementById('blogs-container')
 const viewMoreBtn = document.getElementById('view-more-btn')
+const hamburgerEl = document.getElementById('hamburger')
+const closeBtnEl = document.getElementById('close-btn')
+const headerLinks = document.getElementById('header-links')
+const sidebar = document.getElementById('sidebar')
 console.log(blogsData)
 
 const getBlogsData = () => {
@@ -13,19 +17,23 @@ const getBlogsData = () => {
         if(i < 3){
             blogs += `
             <div class="blog">
-                <img src="./images/${img}" alt="${alt}" width="375" height="250">
-                <span class="date">${date}</span>
-                <h3>${name}</h3>
-                <p class="blog-content">${content}</p>
+                <a href="#">
+                    <img src="./images/${img}" alt="${alt}" width="375" height="250">
+                    <span class="date">${date}</span>
+                    <h3>${name}</h3>
+                    <p class="blog-content">${content}</p>
+                </a>
             </div>
             `
         } else {
             blogs += `
             <div class="blog hide">
-                <img src="./images/${img}" alt="${alt}" width="375" height="250">
-                <span class="date">${date}</span>
-                <h3>${name}</h3>
-                <p class="blog-content">${content}</p>
+                <a href="#">
+                    <img src="./images/${img}" alt="${alt}" width="375" height="250">
+                    <span class="date">${date}</span>
+                    <h3>${name}</h3>
+                    <p class="blog-content">${content}</p>
+                </a>
             </div>
             `
         }
@@ -44,18 +52,37 @@ const getBlogsData = () => {
     // })
     return blogs
 }
-
-viewMoreBtn.addEventListener("click", ()=> {
-    const allBlogs = document.querySelectorAll(".blog")
-    allBlogs.forEach(item => {
-        item.classList.toggle('hide')
-    })
-    
-    viewMoreBtn.innerText === 'View more' ? viewMoreBtn.innerText = 'View less' : viewMoreBtn.innerText = 'View more'
-})
-
 const renderData = () => {
     blogsContainer.innerHTML = getBlogsData()
 }
 
 renderData()
+
+viewMoreBtn.addEventListener("click", ()=> {
+    const allBlogs = document.querySelectorAll(".blog.hide")
+    
+    if(allBlogs.length > 0){
+        allBlogs.forEach(item => {
+            item.classList.remove('hide')
+        })
+    } else {
+        const blogs = document.querySelectorAll('.blog')
+
+        blogs.forEach((item, index) => {
+            if(index >= 3) item.classList.add('hide')
+        })
+    }
+    
+    viewMoreBtn.innerText === 'View more' ? viewMoreBtn.innerText = 'View less' : viewMoreBtn.innerText = 'View more'
+})
+
+
+// sidebar handling
+
+hamburgerEl.addEventListener('click', ()=> {
+    sidebar.style.transform = "translateX(0%)"
+})
+
+closeBtnEl.addEventListener('click', ()=> {
+    sidebar.style.transform = 'translateX(100%)'
+})
